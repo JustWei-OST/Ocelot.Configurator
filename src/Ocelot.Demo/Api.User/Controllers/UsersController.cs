02c7e889a1ebae4e659446ff5fec123cb8366771
吴宏伟 <wlclass@163.com>
2018-05-07 19:08:02 +0800

    Added demo projects

---
+++ src/Ocelot.Demo/Api.User/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.User.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "user1", "user2" };
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "user";
+        }
+
+        // POST api/users
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Ocelot.Demo/README.md
@@ -0,0 +1,29 @@
+# Ocelot演示项目
+
+### 子项目：
+- ApiGateway: 网关 （端口：5000）
+- 服务Api项目
+  - Api.Product:（端口：5001）
+  - Api.User: （端口：5002）
+
+### 网关启用的功能：
+- 启用了Butterfly Tracing
+- 启用了Administration Api
+- 启用了Cors
+
+
+### 启动方式：
+
+- 1。先启动两个Api服务项目
+- 2。再启动网关项目
+
+分别访问以下URL:
+
+- http://localhost:5000/api/users
+- http://localhost:5000/api/users
+
+Administration基地址
+http://localhost:5000/administration
+
+### 测试
+启动三个项目后，可以将 Ocelot.postman_collection.json 文件导入到[postman](https://www.getpostman.com/)中进行测试
